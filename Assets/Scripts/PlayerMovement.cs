0a8d89ff3e85d89e4b82df32d172df1181c36b26
Ohi Ahimie <ohi-ii@att.net>
2020-11-12 09:35:02 -0700

    added second  player and animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        
+    }
+
+    public void OnLanding(){
+        animator.SetBool("isJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, isJumping);
+        isJumping = false;
+    }
+}
+
+    
